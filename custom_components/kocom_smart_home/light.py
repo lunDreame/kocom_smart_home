78e28fee6474432a493ed6901548213ef01055ca
lunDreame <87955512+lunDreame@users.noreply.github.com>
2024-03-26 18:18:08 +0900

    Update light.py

--- custom_components/kocom_smart_home/light.py
+++ custom_components/kocom_smart_home/light.py
@@ -1,174 +1,73 @@
-import logging
-from datetime import timedelta
-from homeassistant.components.light import (
-    LightEntity,
-    LightEntityFeature,
-)
+from homeassistant.components.light import LightEntity, ColorMode
 
-from .const import (
-    DOMAIN,
-    VERSION, 
-    CONF_PHONE_NUMBER,
-    PAIR_INFO
-)
-from .api import parse_device_info
-
-_LOGGER = logging.getLogger(__name__)
-
-SCAN_INTERVAL = timedelta(seconds=120)
+from .const import DOMAIN, LOGGER, BIT_OFF, BIT_ON
+from .coordinator import KocomCoordinator
+from .device import KocomEntity
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    hass_data = hass.data[DOMAIN][config_entry.entry_id]
-    entities = []
+    api = hass.data[DOMAIN][config_entry.entry_id]
+    entities_to_add: list = []
 
-    light_data = hass_data.device_data.get('light')
+    coordinators = [
+        KocomCoordinator("light", api, hass, config_entry),
+        KocomCoordinator("totalcontrol", api, hass, config_entry)
+    ]
 
-    for device in light_data['entry']:
-        for id in device['list']:
-            entities.append(KocomLight(device, id, hass_data))
+    for coordinator in coordinators:
+        devices = await coordinator.get_devices()
+        entities_to_add.extend(
+            KocomLight(coordinator, device) for device in devices
+        )
 
-    totalcontrol_state = await hass_data.check_device_status('totalcontrol')
+    async_add_entities(entities_to_add)
 
-    if isinstance(totalcontrol_state, dict):
-        entities.append(KocomBatchLight(totalcontrol_state, hass_data))
-
-    async_add_entities(entities)
-
-
-class KocomLight(LightEntity):
-    def __init__(self, device_type, device_id, hass_data) -> None:
-        self._device_type = device_type
-        self._device_id = device_id
-        self._hass_data = hass_data
-        self._result = {}
-        self._features = 0
+class KocomLight(KocomEntity, LightEntity):
+    def __init__(self, coordinator, device) -> None:
+        self.device = device
+        self.device_id = device.get('device_id')
+        self.device_name = device.get('device_name')
+        super().__init__(coordinator)
 
     @property
-    def unique_id(self):
+    def unique_id(self) -> str:
         """Return the entity ID."""
-        return f'light.{self._device_type['id'].lower()}_{self._device_id['function'].lower()}_{self._hass_data.entry.data[CONF_PHONE_NUMBER][7:]}'
-
+        return self.device_id
+    
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the sensor, if any."""
-        return f'{self._device_type['id']} {self._device_id['function']}'
-
+        return self.device_name
+    
     @property
-    def is_on(self):
+    def is_on(self) -> bool:
         """If the switch is currently on or off."""
-        return self._hass_data.is_device_state('light', self._device_type['id'], self._device_id['function'])
+        return self.coordinator._is_device_state(self.device_id)
 
     @property
-    def supported_features(self):
-        return LightEntityFeature(self._features)
+    def color_mode(self) -> ColorMode:
+        return ColorMode.ONOFF
+        
+    @property
+    def supported_color_modes(self) -> set[ColorMode]:
+        return ColorMode.ONOFF
 
     @property
     def extra_state_attributes(self):
         """Attributes."""
         attributes = {
-            "Device room": self._device_type['id'],
-            "Device type": self._device_id['function'],
-            "Device state": self._hass_data.is_device_state(
-                'light', self._device_type['id'], self._device_id['function']
-            ),
+            "Device room": self.device['device_room'],
+            "Device type": self.device['device_type'],
+            "Registration Date": self.device['reg_date'],
+            "Sync date": self.coordinator._data['sync_date']
         }
         return attributes
-    
-    async def async_update(self):
-        """Get the latest state of the sensor."""
-        if self._hass_data is None: 
-            return
         
-        await self._hass_data.device_state('light')
-
     async def async_turn_on(self, **kwargs):
         """Turn the switch on."""
-        self._result = await self._hass_data.send_control_request('light', self._device_type['id'], self._device_id['function'], '255')
-
-        self._hass_data.update_device_data(self._result)
-
+        await self.coordinator.set_device_command(self.device_id, BIT_ON)
+        await self.coordinator.async_request_refresh()
+        
     async def async_turn_off(self, **kwargs):
         """Turn the switch off."""
-        self._result = await self._hass_data.send_control_request('light', self._device_type['id'], self._device_id['function'], '0')
-
-        self._hass_data.update_device_data(self._result)
-
-    @property
-    def device_info(self):
-        """Return information about the device."""
-        return {
-            "identifiers": {(DOMAIN, 'light')},
-            "name": "KOCOM Light",
-            "manufacturer": "Kocom Co, Ltd.",
-            "model": self._hass_data.login_data[PAIR_INFO]['list'][0]['alias'],
-            "sw_version": VERSION
-        }
-    
-
-
-class KocomBatchLight(LightEntity):
-    def __init__(self, state_data, hass_data) -> None:
-        self._state_data = state_data
-        self._hass_data = hass_data
-        self._device_type = "일괄소등"
-        self._state_attr = parse_device_info(state_data, 'attr')
-        self._state_power = parse_device_info(state_data, 'totallight')
-        self._result = {}
-        self._features = 0
-
-    @property
-    def unique_id(self):
-        """Return the entity ID."""
-        return f'light.cutoff_switch_{self._state_attr['id'].lower()}_{self._hass_data.entry.data[CONF_PHONE_NUMBER][7:]}'
-
-    @property
-    def name(self):
-        """Return the name of the sensor, if any."""
-        return self._device_type
-
-    @property
-    def is_on(self):
-        """If the switch is currently on or off."""
-        return self._state_power
-    
-    @property
-    def supported_features(self):
-        return LightEntityFeature(self._features)
-
-    @property
-    def extra_state_attributes(self):
-        """Attributes."""
-        attributes = {
-            "Device type": self._state_attr['id'],
-            "Device state": self._state_power,
-            "Registration date": self._state_attr['reg_date']
-        }
-        return attributes
-    
-    def update(self):
-        """Get the latest state of the sensor."""
-        if self._hass_data is None: 
-            return
-
-    async def async_turn_on(self, **kwargs):
-        """Turn the switch on."""
-        self._result = await self._hass_data.send_control_request('totalcontrol', self._state_attr['id'], 'totallight', '1')
-        
-        self._state_power = parse_device_info(self._result, 'totallight')
-
-    async def aync_turn_off(self, **kwargs):
-        """Turn the switch off."""
-        self._result = await self._hass_data.send_control_request('totalcontrol', self._state_attr['id'], 'totallight', '0')
-        
-        self._state_power = parse_device_info(self._result, 'totallight')
-
-    @property
-    def device_info(self):
-        """Return information about the device."""
-        return {
-            "identifiers": {(DOMAIN, 'kocom')},
-            "name": "KOCOM",
-            "manufacturer": "Kocom Co, Ltd.",
-            "model": self._hass_data.login_data[PAIR_INFO]['list'][0]['alias'],
-            "sw_version": VERSION
-        }
+        await self.coordinator.set_device_command(self.device_id, BIT_OFF)
+        await self.coordinator.async_request_refresh()
