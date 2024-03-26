a3a56b32fad7bcde1aed5bb228aeae2a6f7c527f
lunDreame <87955512+lunDreame@users.noreply.github.com>
2024-03-26 18:15:56 +0900

    Update const.py

--- custom_components/kocom_smart_home/const.py
+++ custom_components/kocom_smart_home/const.py
@@ -1,52 +1,20 @@
 """Constants for Kocom Smart Home."""
+import logging
 from homeassistant.const import Platform
 from homeassistant.components.sensor import SensorDeviceClass
 
+LOGGER = logging.getLogger(__name__)
+
 NAME = "Kocom Smart Home"
 DOMAIN = "kocom_smart_home"
-VERSION = "1.0.3"
-PLATFORMS = [
-    Platform.FAN,
-    Platform.LIGHT,
-    Platform.SENSOR,
-    Platform.SWITCH,
-    Platform.CLIMATE,
-]
+VERSION = "1.1.0"
 
-CONF_PHONE_NUMBER  = "phone_number"
-CONF_WALLPAD_NUMBER = "wallpad_number"
-CONF_ROOM_INTERVAL = "room_interval"
-CONF_GAS_INTERVAL = "gas_interval"
-CONF_VENT_INTERVAL = "vent_interval"
-CONF_ENERGY_INTERVAL = "energy_interval"
+TIMEOUT_SEC = 10
 
-TIMEOUT_SEC = 30
+BIT_OFF = 0
+BIT_ON = 1
 
-SPHONE_INFO = "sphone_info"
-SPHONE_UUID = "sphone_uuid"
-PAIR_INFO = "pair_info"
-ZONE_ID = "zone_id"
-
-SPEED_LOW = "1"
-SPEED_MID = "2" 
-SPEED_HIGH = "3" 
-SPEED_LIST = [SPEED_LOW, SPEED_MID, SPEED_HIGH]
-
-FAN_ICONS = {
-    "off": "mdi:fan-off",
-    "1": "mdi:fan-speed-1",
-    "2": "mdi:fan-speed-2",
-    "3": "mdi:fan-speed-3"
-}
-
-ROOM_ICONS = {
-    "light": "mdi:lightbulb",
-    "outlet": "mdi:power-socket-eu",
-    "thermostat": "mdi:thermostat",
-    "aircon": "mdi:air-conditioner"
-}
-
-ENERGY_NAME = {
+ENERGY_INFO = {
     "elec": ["전기 사용량", "mdi:flash", "kWh", SensorDeviceClass.ENERGY, "total_increasing"],
     "heat": ["난방 사용량", "mdi:radiator", "㎥", None, None],
     "hotwater": ["온수 사용량", "mdi:hot-tub", "㎥", None, None],
@@ -54,7 +22,7 @@ ENERGY_NAME = {
     "water": ["수도 사용량", "mdi:water-pump", "㎥", SensorDeviceClass.WATER, "total_increasing"]
 }
 
-ENERGY_UNIT_NAME = {
+ENERGY_UNIT = {
     "value": "우리집",
     "avg": "이번달 평균",
     "price": "예상 요금",
@@ -63,4 +31,11 @@ ENERGY_UNIT_NAME = {
     "_previousprice": "전월 예상 요금",   
 }
 
+PLATFORMS = [
+    Platform.FAN,
+    Platform.LIGHT,
+    Platform.SENSOR,
+    Platform.SWITCH,
+    Platform.CLIMATE,
+]
 
