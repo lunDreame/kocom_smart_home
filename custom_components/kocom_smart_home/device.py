1c5afdf7b03cd584b700ad9698d8b6eec35fbaae
lunDreame <87955512+lunDreame@users.noreply.github.com>
2024-03-26 18:17:28 +0900

    Create device.py

---
+++ custom_components/kocom_smart_home/device.py
@@ -0,0 +1,15 @@
+"""Device class."""
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.helpers.entity import DeviceInfo
+
+from .const import DOMAIN, VERSION, LOGGER
+from .coordinator import KocomCoordinator
+
+class KocomEntity(CoordinatorEntity[KocomCoordinator]):
+    """Defines a base Kocom entity."""
+    
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return device information about this Kocom device."""
+        return self.coordinator.get_device_info()
+    
