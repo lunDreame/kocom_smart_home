950185c0b5e534af9ae8564512ceebb2b50c05ca
lunDreame <87955512+lunDreame@users.noreply.github.com>
2024-04-04 21:01:56 +0900

    Update sensor.py

--- custom_components/kocom_smart_home/sensor.py
+++ custom_components/kocom_smart_home/sensor.py
@@ -31,8 +31,8 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 class KocomSensor(KocomEntity, SensorEntity):
     def __init__(self, coordinator, device) -> None:
         self.device = device
-        self.device_id = device.get('device_id')
-        self.device_name = device.get('device_name')
+        self.device_id = device.get("device_id")
+        self.device_name = device.get("device_name")
         super().__init__(coordinator)
 
     @property
@@ -42,44 +42,48 @@ class KocomSensor(KocomEntity, SensorEntity):
     
     @property
     def name(self) -> str:
-        """Return the name of the sensor, if any."""
+        """Return the name of the device."""
         return self.device_name
 
     @property
     def icon(self):
-        """Icon to use in the frontend, if any."""
-        return ENERGY_INFO[self.device['device_room']][1]
+        """Return the icon of the sensor."""
+        return ENERGY_INFO[self.device["device_room"]][1]
 
     @property
     def unit_of_measurement(self):
-        if self.device['device_type'] == "price":
+        """Return the unit of measurement of this sensor."""
+        if self.device["device_type"] == "price":
             return "KRW/kWh"
-        return ENERGY_INFO[self.device['device_room']][2]
+        return ENERGY_INFO[self.device["device_room"]][2]
 
     @property
     def device_class(self):
-        return ENERGY_INFO[self.device['device_room']][3]
+        """Return the class of the sensor."""
+        return ENERGY_INFO[self.device["device_room"]][3]
     
     @property
     def state_class(self):
-        _state_class = ENERGY_INFO[self.device['device_room']][4]    
-        if _state_class is not None and self.device['is_prev_month']:
-            return _state_class.split('_')[0]
+        """Type of this sensor state."""
+        _state_class = ENERGY_INFO[self.device["device_room"]][4]    
+        if _state_class is not None and self.device["is_prev_month"]:
+            return _state_class.split("_")[0]
         return _state_class
         
     @property
     def state(self):
+        """Return the state of the sensor."""
         return self.coordinator._energy_usage_state(
-            self.device_id, self.device['reg_date']
+            self.device_id, self.device["reg_date"]
         )
     
     @property
     def extra_state_attributes(self):
-        """Attributes."""
-        attributes = {
-            "Device room": self.device['device_room'],
-            "Device type": self.device['device_type'],
-            "Registration Date": self.device['reg_date'],
-            "Sync date": self.coordinator._data['sync_date']
+        """Return the state attributes of the sensor."""
+        return {
+            "Device room": self.device["device_room"],
+            "Device type": self.device["device_type"],
+            "Registration Date": self.device["reg_date"],
+            "Sync date": self.coordinator._data["sync_date"]
         }
-        return attributes
+
