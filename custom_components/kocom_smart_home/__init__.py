94b92c686e13080bfe09fde87cbd4dc07eeebb02
lunDreame <87955512+lunDreame@users.noreply.github.com>
2024-04-04 20:58:47 +0900

    Update __init__.py

--- custom_components/kocom_smart_home/__init__.py
+++ custom_components/kocom_smart_home/__init__.py
@@ -1,46 +1,37 @@
 """
 Custom integration to integrate Kocom Smart Home with Home Assistant.
 """
-import asyncio
 
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN, PLATFORMS, LOGGER
 from .api import KocomHomeAPI
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up this integration using UI."""
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Integration setup."""
     hass.data.setdefault(DOMAIN, {})
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up this integration using UI."""
+    """Set up a config entry."""
     hass.data.setdefault(DOMAIN, {})
 
-    entry_data: dict[str] = entry.data
-    LOGGER.debug("async_setup_entry. entry_data :: %s", entry_data)
-    
-    api = KocomHomeAPI(hass)
+    api: KocomHomeAPI = KocomHomeAPI(hass)
     await api.set_entry_and_initialize_devices(entry)
 
     hass.data[DOMAIN][entry.entry_id] = api
-
-    for component in PLATFORMS:
-        hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, component))
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
         
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Handle removal of an entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
-    return unload_ok
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    hass.data[DOMAIN].pop(entry.entry_id)
+
+    return True
 
     
